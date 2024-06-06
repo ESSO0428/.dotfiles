fb3062be1715ff62f3cae51ac9b17628865a0176
ESSO0428 <92996726+ESSO0428@users.noreply.github.com>
2024-06-06 23:57:20 +0800

    Update LoadMyVimConfigOnly.sh

--- LoadMyVimConfigOnly.sh
+++ LoadMyVimConfigOnly.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 mv ~/.vimrc ~/.vimrc.old > /dev/null 2>&1
+mkdir -p ~/.vim/vim/
 rm -rf ~/.vim/vim.old/
 mv ~/.vim/vim/ ~/.vim/vim.old/ > /dev/null 2>&1
 cp ./.vimrc ~/.vimrc
 cp -R ./.vim/vim/ ~/.vim/vim/
-
 
